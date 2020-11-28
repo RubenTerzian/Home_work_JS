cf12d1b009ef8978baf05c5c4cd61d2c43d7e530
Ruben <rbr2063@gmail.com>
2020-11-28 23:21:14 +0200

    hw1-1

---
+++ index.js
@@ -0,0 +1,14 @@
+//  1 EX
+
+for( i = 1; i <= 10; i++){
+    if (i%3 == 0){
+      let i = 'FizBuz'
+      console.log(i)
+    }else if( i%2 == 0){
+       let i = 'Fiz'
+       console.log(i)
+    }else {
+        let i = 'Buz'
+        console.log(i)
+    }
+ }
