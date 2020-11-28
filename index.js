6ce7518f900d2cd430b5752e7d89870d45c03827
Ruben <rbr2063@gmail.com>
2020-11-29 00:21:22 +0200

    hw1-3

--- index.js
+++ index.js
@@ -15,10 +15,26 @@ for( i = 1; i <= 10; i++){
 
  // 2 EX
 
-let number = 0
+let number = 10
 
 for( let i = number-1; i >= 1; i--){
    number*=i
     }
 
 console.log(number)
+
+// 3 EX
+
+const sheetsInReamPaper = 500;
+const consumptionPerWeek = 1200;
+const weeksAmount = 10;
+
+let result = consumptionPerWeek*weeksAmount;
+let finalResult;
+
+if(result%sheetsInReamPaper > 0){
+finalResult = Math.ceil(result/sheetsInReamPaper)
+}else{
+    finalResult = result/sheetsInReamPaper
+}
+console.log(finalResult)
