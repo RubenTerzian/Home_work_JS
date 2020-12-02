0a7dd28c96c3ad20bab0e30843c2918c8a71f099
Ruben <rbr2063@gmail.com>
2020-12-02 18:37:46 +0200

    hw2-2

--- index.js
+++ index.js
@@ -13,6 +13,30 @@ const citiesAndCountries = {
 const arr =[];
 
     for(let result in citiesAndCountries)
-    arr.push(result+' - это '+citiesAndCountries[result]);
+    arr.push(result+' - это '+citiesAndCountries[result])
 
 console.log(arr)
+
+// hw2-2
+
+function getArray(){
+    const amount = 15
+    const arr =[]
+    const arrInside = [1, 2, 3]
+    if(amount%3==0){
+        for(i=0;  i<=(amount/3-1); i++){
+                if(i>0){
+                    for(j=0;  j<=2; j++){
+                        arrInside[j] +=3 
+                    }
+                }
+            arr[i] = arrInside+''
+        }
+        console.log(arr)
+        
+    }else{
+        console.log('amount должно быть кратно 3')
+    }
+}
+
+getArray ()
