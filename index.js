82fda2d34cfb269090e4e68ccf98cda03796ba41
Ruben <rbr2063@gmail.com>
2020-12-22 01:14:15 +0200

    hw6-1

--- index.js
+++ index.js
@@ -1,80 +1,9 @@
-// hw5-1
+// hw6-1
+console.log("--------------------- TASK 6-1 -----------------------")
 
-const counterClosure = () =>{
-    let localAmount = 0;
-
-    return (amount)=>{
-        localAmount += amount;
-        return localAmount;  
-    };
-};
-
-const counter = counterClosure();
-
-// hw5-2
-
-const getUpdateArrClosure = () =>{
-    let localArr = [];
-    return (element) =>{
-        if(element == undefined){
-            localArr.splice(0, localArr.length)
-            return localArr;
-        }
-        localArr.push(element);
-        return localArr;
-    }
+const arr = ['Vasya', 'Petya', 'Alexey']
+ removeUser = (a, b)=>{
+    a.splice(b, 1);
 }
-
-const getUpdateArr = getUpdateArrClosure();
-
-// hw5-3
-
-const getTimeClosure = () =>{
-    let localArr = [];
-    return () =>{
-        if(localArr.length == 0){
-            localArr.push(new Date().getTime());
-            return "Enabled";
-        }
-        localArr.push(new Date().getTime());
-        let result = localArr[1] - localArr[0];
-        if(localArr.length>1){
-            localArr.splice(0,1);
-        }   
-        let finalResult = Math.floor(result/1000)
-        return "С последнего вызова функции прошло секунд - " + finalResult;
-    } 
-}
-
-const getTime = getTimeClosure();
-
-// hw5-4
-
-const timer = (time)=>{
-    
-    const foo = (a => _ => a++)(0);
-    const calculation =()=>{
-        let result = time - foo();
-        let minutes = Math.floor(result/60);
-        let seconds = result%60;
-        let minutesView;
-        let secondsView
-        minutes <10? minutesView = "0" + minutes :  minutesView = minutes;
-        seconds <10? secondsView = "0" + seconds :  secondsView = seconds;
-       
-        if(result > 0){
-            console.log(minutesView + ":" + secondsView);
-        }
-        if(result === 0){
-            console.log('Time end')
-            clearInterval(counter);
-        }
-        
-    };
-    const counter = setInterval(calculation, 1000);
-    return "Start";
-    
-};
-
-
-
+removeUser(arr, 1)
+console.log(arr) 
